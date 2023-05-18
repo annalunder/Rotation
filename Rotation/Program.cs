b2919c93b1356f34500eb3b726e2861ed686d6ce
anna_ <anna_@ANNA_HEMMA>
2023-05-18 18:25:16 +0200

    Add project files.

---
+++ Rotation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+public class LeftRotationOfArray
+{
+    static void Main(string[] args)
+    {
+        int[] arr = new int[] { 1, 2, 3, 4, 5 };
+        Console.Write("Originalarray: ");
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.WriteLine();
+
+        LeftRotationOfArray lr = new LeftRotationOfArray();
+        lr.RigthRotate(arr);
+
+        Console.Write("Högerrotation av arrayen med en siffra: ");
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            Console.Write(arr[i] + " ");
+        }
+
+        Console.ReadKey();
+    }
+
+    void RigthRotate(int[] arr)
+    {
+        int x = arr[(arr.Length - 1)];
+        for (int i = (arr.Length - 1); i > 0; i--)
+        {
+            arr[i] = arr[i - 1];
+        }
+
+        arr[0] = x;
+    }
+}
